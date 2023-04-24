78665586c8b9bc9166683fc292533dcc95ca6ee5
Olivier Pouilly <o.pouilly@dlsoftware.fr>
2023-04-24 23:15:02 +0200

    test

--- main.tf
+++ main.tf
@@ -76,3 +76,36 @@ resource "aws_api_gateway_method_response" "_" {
     aws_api_gateway_method._,
   ]
 }
+
+# aws_api_gateway_integration_response._
+resource "aws_api_gateway_integration_response" "__" {
+  rest_api_id = var.api_id
+  resource_id = var.api_resource_id
+  http_method = aws_api_gateway_method._.http_method
+  status_code = 401
+
+  response_parameters = local.integration_response_parameters
+
+  depends_on = [
+    aws_api_gateway_integration._,
+    aws_api_gateway_method_response._,
+  ]
+}
+
+# aws_api_gateway_method_response._
+resource "aws_api_gateway_method_response" "__" {
+  rest_api_id = var.api_id
+  resource_id = var.api_resource_id
+  http_method = aws_api_gateway_method._.http_method
+  status_code = 401
+
+  response_parameters = local.method_response_parameters
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  depends_on = [
+    aws_api_gateway_method._,
+  ]
+}
